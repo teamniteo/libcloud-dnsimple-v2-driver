68b2fa93c1448b96a2b4e52eb07eaf2d8e4be3e0
Adam Štrauch <cx@initd.cz>
2018-08-08 00:38:26 +0200

    Initial commit

---
+++ dnsimple.py
@@ -0,0 +1,360 @@
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+DNSimple v2 DNS Driver
+"""
+from libcloud.common.base import ConnectionUserAndKey
+
+__all__ = [
+    'DNSimpleV2DNSDriver'
+]
+
+try:
+    import simplejson as json
+except ImportError:
+    import json
+
+from libcloud.common.dnsimple import DNSimpleDNSResponse
+from libcloud.dns.types import RecordType
+from libcloud.dns.base import DNSDriver, Zone, Record
+
+
+DEFAULT_ZONE_TTL = 3600
+
+
+class DNSimpleV2DNSConnection(ConnectionUserAndKey):
+    """
+    user_id: put account ID, you can find it here: https://dnsimple.com/a/<ACCOUNT_ID>/account
+    key: API token taken from the admin interface
+    """
+
+    host = 'api.dnsimple.com'
+    responseCls = DNSimpleDNSResponse
+
+    def add_default_headers(self, headers):
+        """
+        Add headers that are necessary for every request
+
+        This method adds ``token`` to the request.
+        """
+        headers['Authorization'] = 'Bearer {}'.format(self.key)
+        headers['Accept'] = 'application/json'
+        headers['Content-Type'] = 'application/json'
+        return headers
+
+
+class DNSimpleV2DNSDriver(DNSDriver):
+    type = "DNSimpleV2"
+    name = 'DNSimpleV2'
+    website = 'https://dnsimple.com/'
+    connectionCls = DNSimpleV2DNSConnection
+
+    RECORD_TYPE_MAP = {
+        RecordType.A: 'A',
+        RecordType.AAAA: 'AAAA',
+        RecordType.ALIAS: 'ALIAS',
+        RecordType.CNAME: 'CNAME',
+        RecordType.HINFO: 'HINFO',
+        RecordType.MX: 'MX',
+        RecordType.NAPTR: 'NAPTR',
+        RecordType.NS: 'NS',
+        'POOL': 'POOL',
+        RecordType.SOA: 'SOA',
+        RecordType.SPF: 'SPF',
+        RecordType.SRV: 'SRV',
+        RecordType.SSHFP: 'SSHFP',
+        RecordType.TXT: 'TXT',
+        RecordType.URL: 'URL'
+    }
+
+    def list_zones(self):
+        """
+        Return a list of zones.
+
+        :return: ``list`` of :class:`Zone`
+        """
+        response = self.connection.request('/v2/{}/domains'.format(self.connection.user_id))
+
+        zones = self._to_zones(response.object)
+        return zones
+
+    def list_records(self, zone):
+        """
+        Return a list of records for the provided zone.
+
+        :param zone: Zone to list records for.
+        :type zone: :class:`Zone`
+
+        :return: ``list`` of :class:`Record`
+        """
+        response = self.connection.request('/v2/{}/zones/{}/records'.format(self.connection.user_id, zone.id))
+        records = self._to_records(response.object, zone)
+        return records
+
+    def get_zone(self, zone_id):
+        """
+        Return a Zone instance.
+
+        :param zone_id: ID of the required zone
+        :type  zone_id: ``str``
+
+        :rtype: :class:`Zone`
+        """
+        response = self.connection.request('/v2/{}/domains/{}'.format(self.connection.user_id, zone_id))
+        zone = self._to_zone(response.object)
+        return zone
+
+    def get_record(self, zone_id, record_id):
+        """
+        Return a Record instance.
+
+        :param zone_id: ID of the required zone
+        :type  zone_id: ``str``
+
+        :param record_id: ID of the required record
+        :type  record_id: ``str``
+
+        :rtype: :class:`Record`
+        """
+        response = self.connection.request('/v2/{}/zones/%s/records/%s'.format(
+            self.connection.user_id,
+            zone_id,
+            record_id,
+        ))
+        record = self._to_record(response.object, zone_id=zone_id)
+        return record
+
+    def create_zone(self, domain, type='master', ttl=None, extra=None):
+        """
+        Create a new zone. This one actually creates a new domain but
+        the zone is created alongside the domain. The zone can't be
+        updated, because there is no way how to change type or TTL.
+
+        :param domain: Domain name (e.g. example.com)
+        :type domain: ``str``
+
+        :param type: not used
+        :type  type: None
+
+        :param ttl: not used
+        :type  ttl: None
+
+        :param extra: not used
+        :type extra: None
+
+        :rtype: :class:`Zone`
+
+        For more info, please see:
+        https://developer.dnsimple.com/v2/domains/
+        """
+        r_json = {'name': domain}
+
+        if extra is not None:
+            r_json.update(extra)
+
+        r_data = json.dumps(r_json)
+
+        response = self.connection.request(
+            '/v2/{}/domains'.format(self.connection.user_id), method='POST', data=r_data)
+        zone = self._to_zone(response.object)
+        return zone
+
+    def create_record(self, name, zone, type, data, extra=None):
+        """
+        Create a new record.
+
+        :param name: Record name without the domain name (e.g. www).
+                     Note: If you want to create a record for a base domain
+                     name, you should specify empty string ('') for this
+                     argument.
+        :type  name: ``str``
+
+        :param zone: Zone where the requested record is created.
+        :type  zone: :class:`Zone`
+
+        :param type: DNS record type (A, AAAA, ...).
+        :type  type: :class:`RecordType`
+
+        :param data: Data for the record (depends on the record type).
+        :type  data: ``str``
+
+        :param extra: Extra attributes (driver specific). (optional)
+        :type extra: ``dict``
+
+        :rtype: :class:`Record`
+        """
+
+        r_json = {
+            'name': name,
+            'type': type,
+            'content': data
+        }
+
+        if extra is not None:
+            r_json.update(extra)
+
+        r_data = json.dumps(r_json)
+
+        response = self.connection.request(
+            '/v2/{}/zones/{}/records'.format(
+                self.connection.user_id,
+                zone.id
+            ),
+            method='POST',
+            data=r_data,
+        )
+        record = self._to_record(response.object, zone=zone)
+        return record
+
+    def update_record(self, record, name, type, data, extra=None):
+        """
+        Update an existing record.
+
+        :param record: Record to update.
+        :type  record: :class:`Record`
+
+        :param name: Record name without the domain name (e.g. www).
+                     Note: If you want to create a record for a base domain
+                     name, you should specify empty string ('') for this
+                     argument.
+        :type  name: ``str``
+
+        :param type: can't be changed in DNSimple's API
+        :type  type: None
+
+        :param data: Data for the record (depends on the record type).
+        :type  data: ``str``
+
+        :param extra: (optional) Extra attributes (driver specific).
+        :type  extra: ``dict``
+
+        :rtype: :class:`Record`
+        """
+        zone = record.zone
+
+        r_json = {
+            'name': name,
+            'content': data
+        }
+
+        if extra is not None:
+            r_json.update(extra)
+
+        r_data = json.dumps({'record': r_json})
+
+        response = self.connection.request(
+            '/v2/{}/zones/{}/records/{}'.format(
+                self.connection.user_id,
+                zone.id,
+                record.id
+            ),
+            method='PUT',
+            data=r_data,
+        )
+        record = self._to_record(response.object, zone=zone)
+        return record
+
+    def delete_zone(self, zone):
+        """
+        Delete a zone.
+
+        Note: This will delete all the records belonging to this zone.
+
+        :param zone: Zone to delete.
+        :type  zone: :class:`Zone`
+
+        :rtype: ``bool``
+        """
+        self.connection.request('/v2/{}/zones/{}'.format(self.connection.user_id, zone.id), method='DELETE')
+        return True
+
+    def delete_record(self, record):
+        """
+        Delete a record.
+
+        :param record: Record to delete.
+        :type  record: :class:`Record`
+
+        :rtype: ``bool``
+        """
+        zone_id = record.zone.id
+
+        self.connection.request('/v2/{}/zones/%s/records/%s'.format(
+            self.connection.user_id,
+            zone_id,
+            record.id,
+        ), method='DELETE')
+
+        return True
+
+    def _to_zones(self, data):
+        # TODO: upgrade this to v2
+        zones = []
+        for zone in data.get("data"):
+            _zone = self._to_zone(zone)
+            zones.append(_zone)
+
+        return zones
+
+    def _to_zone(self, data):
+        # TODO: upgrade this to v2
+        id = data.get('id')
+        name = data.get('name')
+        extra = {
+            "account_id": data.get("account_id"),
+            "registrant_id": data.get("registrant_id"),
+            "unicode_name": data.get("unicode_name"),
+            "state": data.get("state"),
+            "auto_renew": data.get("auto_renew"),
+            "private_whois": data.get("private_whois"),
+            "expires_on": data.get("expires_on"),
+            "created_at": data.get("created_at"),
+            "updated_at": data.get("updated_at")
+        }
+
+        # All zones are primary by design
+        type = 'master'
+
+        return Zone(id=id, domain=name, type=type, ttl=DEFAULT_ZONE_TTL,
+                    driver=self, extra=extra)
+
+    def _to_records(self, data, zone):
+        # TODO: upgrade this to v2
+        records = []
+        for item in data.get("data"):
+            record = self._to_record(item, zone=zone)
+            records.append(record)
+        return records
+
+    def _to_record(self, data, zone_id=None, zone=None):
+        # TODO: upgrade this to v2
+        if not zone:  # We need zone_id or zone
+            zone = self.get_zone(zone_id)
+        id = data.get('id')
+        name = data.get('name')
+        type = data.get('type')
+        content = data.get('content')
+        extra = {
+            "zone_id": data.get("zone_id"),
+            "parent_id": data.get("parent_id"),
+            "ttl": data.get("ttl"),
+            "priority": data.get("priority"),
+            "regions": data.get("regions"),
+            "system_record": data.get("system_record"),
+            "created_at": data.get("created_at"),
+            "updated_at": data.get("updated_at"),
+        }
+        return Record(id=id, name=name, type=type, data=content, zone=zone,
+                      driver=self, ttl=data.get('ttl', None), extra=extra)
---
+++ test_dnsimple.py
@@ -0,0 +1,180 @@
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+import sys
+import unittest
+
+from libcloud.utils.py3 import httplib
+
+from libcloud.dns.types import RecordType
+
+from libcloud.test import MockHttp
+from libcloud.test.file_fixtures import FileFixtures
+
+from dnsimple import DNSimpleV2DNSDriver
+
+
+DNS_PARAMS_DNSIMPLE_V2 = ('user', 'key')
+
+
+class DNSimpleV2DNSTests(unittest.TestCase):
+    def setUp(self):
+        DNSimpleV2DNSDriver.connectionCls.conn_class = DNSimpleV2DNSMockHttp
+        DNSimpleV2DNSMockHttp.type = None
+        self.driver = DNSimpleV2DNSDriver(*DNS_PARAMS_DNSIMPLE_V2)
+
+    def assertHasKeys(self, dictionary, keys):
+        for key in keys:
+            self.assertTrue(key in dictionary, 'key "%s" not in dictionary' %
+                            (key))
+
+    def test_list_record_types(self):
+        record_types = self.driver.list_record_types()
+        self.assertEqual(len(record_types), 15)
+        self.assertTrue(RecordType.A in record_types)
+        self.assertTrue(RecordType.AAAA in record_types)
+        self.assertTrue(RecordType.ALIAS in record_types)
+        self.assertTrue(RecordType.CNAME in record_types)
+        self.assertTrue(RecordType.HINFO in record_types)
+        self.assertTrue(RecordType.MX in record_types)
+        self.assertTrue(RecordType.NAPTR in record_types)
+        self.assertTrue(RecordType.NS in record_types)
+        self.assertTrue('POOL' in record_types)
+        self.assertTrue(RecordType.SPF in record_types)
+        self.assertTrue(RecordType.SOA in record_types)
+        self.assertTrue(RecordType.SRV in record_types)
+        self.assertTrue(RecordType.SSHFP in record_types)
+        self.assertTrue(RecordType.TXT in record_types)
+        self.assertTrue(RecordType.URL in record_types)
+
+    def test_list_zones_success(self):
+        zones = self.driver.list_zones()
+        self.assertEqual(len(zones), 2)
+
+        zone1 = zones[0]
+        self.assertEqual(zone1.id, '1')
+        self.assertEqual(zone1.type, 'master')
+        self.assertEqual(zone1.domain, 'example-alpha.com')
+        self.assertEqual(zone1.ttl, 3600)
+        self.assertHasKeys(zone1.extra, ["account_id", "registrant_id", "unicode_name", "state", "auto_renew",
+                                         "private_whois", "expires_on", "created_at", "updated_at"])
+
+        zone2 = zones[1]
+        self.assertEqual(zone2.id, '2')
+        self.assertEqual(zone2.type, 'master')
+        self.assertEqual(zone2.domain, 'example-beta.com')
+        self.assertEqual(zone2.ttl, 3600)
+        self.assertHasKeys(zone2.extra, ["account_id", "registrant_id", "unicode_name", "state", "auto_renew",
+                                         "private_whois", "expires_on", "created_at", "updated_at"])
+
+    def test_list_records_success(self):
+        zone = self.driver.list_zones()[0]
+        records = self.driver.list_records(zone=zone)
+        self.assertEqual(len(records), 5)
+
+        record1 = records[0]
+        self.assertEqual(record1.id, '1')
+        self.assertEqual(record1.name, '')
+        self.assertEqual(record1.type, RecordType.SOA)
+        self.assertEqual(record1.data, 'ns1.dnsimple.com admin.dnsimple.com 1458642070 86400 7200 604800 300')
+        self.assertHasKeys(record1.extra, ["zone_id", "parent_id", "ttl", "priority", "regions", "system_record",
+                                           "created_at", "updated_at"])
+
+        record2 = records[1]
+        self.assertEqual(record2.id, '69061')
+        self.assertEqual(record2.name, '')
+        self.assertEqual(record2.type, RecordType.NS)
+        self.assertEqual(record2.data, 'ns1.dnsimple.com')
+        self.assertHasKeys(record2.extra, ["zone_id", "parent_id", "ttl", "priority", "regions", "system_record",
+                                           "created_at", "updated_at"])
+
+        record3 = records[2]
+        self.assertEqual(record3.id, '2')
+        self.assertEqual(record3.name, '')
+        self.assertEqual(record3.type, RecordType.NS)
+        self.assertEqual(record3.data, 'ns2.dnsimple.com')
+        self.assertHasKeys(record3.extra, ["zone_id", "parent_id", "ttl", "priority", "regions", "system_record",
+                                           "created_at", "updated_at"])
+
+        record4 = records[3]
+        self.assertEqual(record4.id, '3')
+        self.assertEqual(record4.name, '')
+        self.assertEqual(record4.type, RecordType.NS)
+        self.assertEqual(record4.data, 'ns3.dnsimple.com')
+        self.assertHasKeys(record4.extra, ["zone_id", "parent_id", "ttl", "priority", "regions", "system_record",
+                                           "created_at", "updated_at"])
+
+        record5 = records[4]
+        self.assertEqual(record5.id, '4')
+        self.assertEqual(record5.name, '')
+        self.assertEqual(record5.type, RecordType.NS)
+        self.assertEqual(record5.data, 'ns4.dnsimple.com')
+        self.assertHasKeys(record5.extra, ["zone_id", "parent_id", "ttl", "priority", "regions", "system_record",
+                                           "created_at", "updated_at"])
+
+
+    # def test_create_record_success(self):
+    #     zone = self.driver.list_zones()[0]
+    #     DNSimpleDNSMockHttp.type = 'CREATE'
+    #     record = self.driver.create_record(name='domain4', zone=zone,
+    #                                        type=RecordType.MX,
+    #                                        data='mail.example.com')
+    #     self.assertEqual(record.id, '172')
+    #     self.assertEqual(record.name, '')
+    #     self.assertEqual(record.type, RecordType.MX)
+    #     self.assertEqual(record.data, 'mail.example.com')
+    #     self.assertHasKeys(record.extra, ['ttl', 'created_at', 'updated_at',
+    #                                       'domain_id', 'priority'])
+
+    # def test_update_record_success(self):
+    #     record = self.driver.get_record(zone_id='1',
+    #                                     record_id='123')
+    #     DNSimpleDNSMockHttp.type = 'UPDATE'
+    #     extra = {'ttl': 4500}
+    #     record1 = self.driver.update_record(record=record, name='www',
+    #                                         type=record.type,
+    #                                         data='updated.com',
+    #                                         extra=extra)
+    #     self.assertEqual(record.data, 'example.com')
+    #     self.assertEqual(record.extra.get('ttl'), 3600)
+    #     self.assertEqual(record1.data, 'updated.com')
+    #     self.assertEqual(record1.extra.get('ttl'), 4500)
+
+    # def test_delete_zone_success(self):
+    #     zone = self.driver.list_zones()[0]
+    #     DNSimpleDNSMockHttp.type = 'DELETE_200'
+    #     status = self.driver.delete_zone(zone=zone)
+    #     self.assertTrue(status)
+
+
+class DNSimpleV2Fixtures(FileFixtures):
+
+    def __init__(self, fixtures_type, sub_dir=''):
+        super().__init__(fixtures_type, sub_dir)
+        self.root = "fixtures"
+
+
+class DNSimpleV2DNSMockHttp(MockHttp):
+    fixtures = DNSimpleV2Fixtures("dns")
+
+    def _v2_user_domains(self, method, url, body, headers):
+        body = self.fixtures.load('list_domains.json')
+        return (httplib.OK, body, {}, httplib.responses[httplib.OK])
+
+    def _v2_user_zones_1_records(self, method, url, body, headers):
+        body = self.fixtures.load('list_records.json')
+        return (httplib.OK, body, {}, httplib.responses[httplib.OK])
+
+
+if __name__ == '__main__':
+    sys.exit(unittest.main())
